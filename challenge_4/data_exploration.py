9d5d570d8e519e33a1c5d06330063cc4eab0ecd7
Jayden Cho - MacBook Pro <kyutcho@umich.edu>
2021-01-04 19:49:49 -0800

    EDA for price feature

--- challenge_4/data_exploration.py
+++ challenge_4/data_exploration.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import missingno as msno
 
 #%%
 pd.set_option('display.max_rows', 500)
@@ -16,27 +17,37 @@ pd.set_option('display.width', 1000)
 #
 # train_data[train_data.columns[[15, 24, 33, 38, 42, 43, 51]]]
 
-#%%
+#%% 
 # Row 17613 causes a problem - Read csv file again
-train_data = pd.read_csv("data/public_listings.csv", skiprows=[17614], parse_dates=["first_review", "last_review"])
+train_raw = pd.read_csv("data/public_listings.csv", skiprows=[17614], parse_dates=["first_review", "last_review"])
+
+#%% 
+# Make copy
+train_data = train_raw.copy()
 
 #%% 
 # Check the first 3 rows
 train_data.head(3)
 
-#%%
+#%% 
 # Columns
 train_data.columns
 
-#%%
+#%% 
 # Shape
 train_data.shape
 
-#%%
+#%% 
 # Basic info
 train_data.info()
 
-#%%
+#%% 
+# Columns that need to be excluded
+excl_cols = ["name", "description", "neighborhood_overview", "picture_url", "host_name",\
+             "host_location", "host_about", "host_thumbnail_url", "host_picture_url"]
+train_data.loc[:, excl_cols]
+
+#%% 
 # Function for missing values
 def calc_missing(df): 
     total = df.isnull().sum().sort_values(ascending=False)
@@ -44,9 +55,39 @@ def calc_missing(df):
 
     missing_df = pd.concat([total, pct], axis=1, keys=["Total", "Percent"]).reset_index()
 
-    return missing_df
+    print(missing_df)
 
-missing_data = calc_missing(train_data)
+calc_missing(train_data)
+
+#%% 
+# Data Transformation (price)
+train_data["price"] = train_data["price"].str.replace(',', '').astype(float)
+
+#%% 
+# Data Transformation (Binary)
+for var in ['has_availability', 'instant_bookable', 'host_is_superhost', 'host_has_profile_pic', 'host_identity_verified']:
+    train_data[var] = train_data[var].map({'t':1, 'f':0})
+
+#%%
+# Data Transformation (Percent)
+train_data["host_response_rate"] = train_data["host_response_rate"].str.replace('%', '').astype(float)
+train_data["host_acceptance_rate"] = train_data["host_acceptance_rate"].str.replace('%', '').astype(float)
+
+#%%
+# EDA (price)
+train_data["price"].describe()
+#%%
+ax = sns.histplot(data = train_data, x = "price")
+ax.set(xlim = (0,2000))
+plt.show()
+
+#%%
+sns.relplot(kind = "scatter", y = train_data["price"], x = train_data["beds"])
+
+#%%
+# sns.scatterplot(x = train_data["longitude"], y = train_data["latitude"])
+# train_data.plot(kind = "scatter", x = "longitude", y = "latitude", 
+# alpha = 0.4, c = "price", cmap=plt.get_cmap("jet"), colorbar=True)
 
 #%%
 # Missing values in host
