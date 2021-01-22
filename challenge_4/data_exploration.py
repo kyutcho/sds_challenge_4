874fd05df4dc6a99051a25d15af70c5fa26aa3af
Jayden Cho - HP Omen <kyutcho@umich.edu>
2021-01-21 18:01:40 -0800

    changed a little bit of EDA

--- challenge_4/data_exploration.py
+++ challenge_4/data_exploration.py
@@ -7,7 +7,8 @@ import missingno as msno
 import sys
 from sklearn.impute import SimpleImputer
 from sklearn.impute import KNNImputer
-from sklearn.impute import IterativeImputer
+# from sklearn.impute import IterativeImputer
+from sklearn.experimental import enable_iterative_imputer
 
 # %%
 pd.set_option('display.max_rows', 500)
@@ -102,17 +103,6 @@ for var in ['has_availability', 'instant_bookable', 'host_is_superhost', 'host_h
 train_data["host_response_rate"] = train_data["host_response_rate"].str.replace('%', '').astype(float)
 train_data["host_acceptance_rate"] = train_data["host_acceptance_rate"].str.replace('%', '').astype(float)
 
-# Imputing
-# %% Categorical vars Manual Impute
-train_data.fillna({"host_response_time": "no response data"}, inplace=True)
-train_data.fillna({"host_verifications": "", "amenities": ""}, inplace=True)
-
-# %%
-# KNN Imputer
-knn_train_copy = train_data.select_dtypes(include="number").copy(deep=True)
-knn_imputer = KNNImputer(n_neighbors=2, weights="uniform")
-knn_train_imputed = knn_imputer.fit_transform(knn_train_copy)
-
 # Feature Engineering
 # %%
 # host_since
@@ -127,7 +117,7 @@ train_data.drop(["last_review", "first_review"], axis=1, inplace=True)
 # %%
 # number of host_verification
 train_data["num_host_verifications"] = train_data["host_verifications"].str.split(", ").apply(lambda x: len(x))
-train_data.drop(["host_verifications"], axis=1, inplace=True)
+# train_data.drop(["host_verifications"], axis=1, inplace=True)
 
 # %%
 # number of amenities
@@ -160,6 +150,24 @@ ctab_chk_2 = ctab_2.apply(lambda x: ((x != 0).sum()), axis=1)
 # Verified overall property_type is well assigned to room_type -> Drop property_type
 train_data.drop(["property_type"], axis=1, inplace=True)
 
+# Imputing
+# %% Categorical vars Manual Impute
+train_data.fillna({"host_response_time": "no response data"}, inplace=True)
+train_data.fillna({"host_verifications": "", "amenities": ""}, inplace=True)
+
+# %%
+# KNN Imputer
+# knn_train_copy = train_data.select_dtypes(include="number").copy(deep=True)
+# knn_imputer = KNNImputer(n_neighbors=2, weights="uniform")
+# knn_train_imputed = knn_imputer.fit_transform(knn_train_copy)
+
+# %%
+# Simple Imputer
+mean_train_copy = train_data.select_dtypes(include="number").copy(deep=True)
+mean_imputer = SimpleImputer(missing_values=np.nan, strategy="mean")
+mean_train_imputed = mean_imputer.fit_transform(mean_train_copy)
+mean_train_imputed = pd.DataFrame(mean_train_imputed)
+
 # %%
 # Beds per bedrooms
 train_data["beds_per_rooms"] = train_data["beds"] / train_data["bedrooms"]
@@ -167,7 +175,6 @@ train_data["beds_per_rooms"] = train_data["beds"] / train_data["bedrooms"]
 # EDA (price)
 # %%
 train_data["price"].describe()
-
 
 # %%
 def calc_IQR(col):
@@ -186,11 +193,9 @@ def calc_outlier(df, col):
 
     return outliers, n_outliers
 
-
 # %%
 # number of outliers
 price_outliers, price_n_outliers = calc_outlier(train_data, "price")
-
 
 # %%
 def df_no_outlier(df, col):
@@ -213,17 +218,18 @@ plt.show()
 
 # %%
 # log(price)
-fig, ax = plt.subplots(2, 2, sharex=True, sharey=True)
-fig.suptitle("This is the super title")
-train_data["log_price"] = np.log(train_data["price"] + 0.00000000001)
-sns.distplot(train_data["log_price"], ax=ax[1, 1])
-ax[1, 1].set(title="This is title")
+epsilon = pow(10, -7)
+fig, ax = plt.subplots()
+sns.distplot(np.log(train_data["price"] + epsilon), ax=ax, bins=50)
+ax.set(title="Distribution with log(price)")
 plt.show()
 
 # %%
 # sqrt(price)
-train_data["sqrt_price"] = np.sqrt(train_data["price"])
-sns.distplot(train_data["sqrt_price"])
+fig, ax = plt.subplots()
+# train_data["log_price"] = np.log(train_data["price"] + 0.00000000001)
+sns.distplot(np.sqrt(train_data["price"]), ax=ax)
+ax.set(title="Distribution with sqrt(price)")
 plt.show()
 
 # %%
